9ee61378456872667e5853297e2747a8b50f994c
travis-ci <travis@travis-ci.org>
2014-09-05 09:15:59 +0000

    Lastest javadoc on successful travis build 44 auto-pushed to gh-pages

--- apidocs/fm/audiobox/core/net/Upload.java
+++ apidocs/fm/audiobox/core/net/Upload.java
@@ -1,7 +1,6 @@
 package fm.audiobox.core.net;
 public class Upload
 {
-public  Upload(fm.audiobox.core.Client client, java.io.File file) { throw new RuntimeException("Stub!"); }
 public  Upload(fm.audiobox.core.Client client, java.io.File file, fm.audiobox.core.net.NetworkProgressListener listener) { throw new RuntimeException("Stub!"); }
 public  fm.audiobox.core.net.NetworkProgressListener getListener() { throw new RuntimeException("Stub!"); }
 public  void setListener(fm.audiobox.core.net.NetworkProgressListener listener) { throw new RuntimeException("Stub!"); }
