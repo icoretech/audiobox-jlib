bbb7830c9ba1f1e83087e116546256b08c2226d3
Valerio Chiodino <valerio.chiodino@openmindonline.it>
2016-03-14 17:52:15 +0100

    Copyright year and other fixes

--- src/test/java/fm/audiobox/tests/unit/base/AudioBoxTests.java
+++ src/test/java/fm/audiobox/tests/unit/base/AudioBoxTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2014 iCoreTech, Inc.
+ * Copyright 2009-2016 iCoreTech, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,7 +44,6 @@ import static org.junit.Assert.fail;
 /**
  * Generic test case class.
  */
-@SuppressWarnings( "deprecation" )
 public class AudioBoxTests {
 
   protected static final File DATA_STORE_DIR = new File( System.getProperty( "user.home" ), ".audiobox/abx" );
